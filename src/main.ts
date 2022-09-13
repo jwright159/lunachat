a30833b0cf03f870e0448b953d85d275f11c956f
Jade Wright <iamjwright159@gmail.com>
2022-09-13 09:30:09 -0400

    I don't know what I'm doing

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { app, BrowserWindow } from "electron";
-import * as path from "path";
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
 
 function createWindow() {
 	// Create the browser window.
@@ -7,15 +7,15 @@ function createWindow() {
 		width: 800,
 		height: 600,
 		webPreferences: {
-			preload: path.join(__dirname, "preload.js"),
+			preload: path.join(__dirname, 'preload.js'),
 		},
 	});
 
 	// and load the index.html of the app.
-	mainWindow.loadFile(path.join(__dirname, "../index.html"));
+	mainWindow.loadFile(path.join(__dirname, '../index.html'));
 
 	// Open the DevTools.
-	// mainWindow.webContents.openDevTools();
+	mainWindow.webContents.openDevTools();
 }
 
 // This method will be called when Electron has finished
@@ -24,7 +24,7 @@ function createWindow() {
 app.whenReady().then(() => {
 	createWindow();
 
-	app.on("activate", function () {
+	app.on('activate', function () {
 		// On macOS it's common to re-create a window in the app when the
 		// dock icon is clicked and there are no other windows open.
 		if (BrowserWindow.getAllWindows().length === 0) createWindow();
@@ -34,11 +34,11 @@ app.whenReady().then(() => {
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on("window-all-closed", () => {
-	if (process.platform !== "darwin") {
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
 		app.quit();
 	}
 });
 
-// In this file you can include the rest of your app"s specific main process
+// In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
--- src/renderer.ts
+++ src/renderer.ts
@@ -4,3 +4,28 @@
 // nodeIntegration is set to true in webPreferences.
 // Use preload.js to selectively enable features
 // needed in the renderer process.
+
+// ---- TYPES
+type VersionKey = 'node' | 'chrome' | 'electron';
+
+declare global {
+	interface Window {
+		electron: {
+			versions: Record<VersionKey, () => string>,
+		}
+	}
+}
+
+// ---- CODE
+import * as $ from 'jquery';
+
+$(() => {
+	const replaceText = (selector: string, text: string) => {
+		const element = $(`#${selector}`);
+		if (element)
+			element.text(text);
+	};
+
+	for (const app of ['node', 'chrome', 'electron'] as VersionKey[])
+		replaceText(`${app}-version`, window.electron.versions[app]());
+});
